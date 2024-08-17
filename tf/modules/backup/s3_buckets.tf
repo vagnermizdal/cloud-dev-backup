c26bf658475187da642af22953f1cf8a980b7798
Vagner Rigon Mizdal <vagner.mizdal@gmail.com>
2024-08-17 10:41:49 -0300

    Added force destroy to buckets on terraform

--- tf/modules/backup/s3_buckets.tf
+++ tf/modules/backup/s3_buckets.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "original_repo" {
   bucket = var.original_bucket
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "backup_repo" {
   bucket = var.backup_bucket
+  force_destroy = true
 }
